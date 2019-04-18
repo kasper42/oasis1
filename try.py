6856e79cc41f89f2bdc5cec57fe2eeb2b17f339f
kasper42 <33011563+kasper42@users.noreply.github.com>
2019-04-18 17:15:12 +0700

    Add files via upload

---
+++ try.py
@@ -0,0 +1,48 @@
+import time
+from pprint import pprint
+from zapv2 import ZAPv2
+
+api = '9dk3g223t3pk57oubeer7h3bc9'
+local = {"http":"http://127.0.0.1:8080", "https":"http://127.0.0.1:8080"}
+target = 'http://bwapp.blackbox.id'
+
+isNewSession = True
+sessionName = 'OASIS'
+
+zap = ZAPv2(apikey=api)
+
+print 'Accessing target %s' %target
+zap.urlopen(target)
+time.sleep(2)
+
+print 'Spidering target %s' %target
+scanid = zap.spider.scan(target)
+time.sleep(2)
+while (int(zap.spider.status(scanid))<100):
+	print 'Spider progress %: ' + zap.spider.status(scanid)
+	time.sleep(2)
+print 'Spider completed'
+time.sleep(5)
+
+print 'Scanning target %s' % target
+scanid = zap.ascan.scan(target)
+while (int(zap.ascan.status(scanid)) < 100):
+    print 'Scan progress %: ' + zap.ascan.status(scanid)
+    time.sleep(5)
+print 'Scan completed'
+
+print ('Hosts: ' + ', '.join(zap.core.hosts))
+print ('Sites: ' + ', '.join(zap.core.sites))
+print ('Urls: ' + ', '.join(zap.core.urls))
+print ('Alerts: ')
+
+pprint (zap.core.alerts())
+
+# Writes the XML and HTML reports that will be exported to the workspace.
+f = open('${workspace}/xmlreport.xml','w')
+f2 = open('${workspace}/htmlreport.html','w')
+f.write(zap.core.xmlreport(apikey = api))
+f2.write(zap.core.htmlreport(apikey = api))
+
+f.close()
+f2.close()
